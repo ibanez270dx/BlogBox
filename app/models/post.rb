913803d9921926e95860d0321e526b181857a68b
Jeff Miller <loadeddesigns@gmail.com>
2015-03-15 14:59:51 -0700

    Add validations to Post, display error messages on form. Make header title link back to root.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
 
-  
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :body, presence: true
+  validates :abstract, presence: true
 
 end
