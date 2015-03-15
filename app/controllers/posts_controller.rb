ad60ef4f3a31083c64968a4e81a2a6d6c43680d4
Jeff Miller <loadeddesigns@gmail.com>
2015-03-15 08:25:31 -0700

    Add new Post form, link in header.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @post = Post.find params[:id]
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      flash[:success] = "Post created successfully!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :author, :author_photo_url, :body, :abstract)
+    end
+
 end
